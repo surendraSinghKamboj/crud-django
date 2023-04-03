36fbe3044e54c08ec82e1dbd7c5b3570be18940a
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-04-03 20:54:32 +0530

    contact page created

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.index,name="home"),
-    path("about",views.about,name="about")
+    path("about",views.about,name="about"),
+    path("contact",views.contact,name="contact")
 ]
